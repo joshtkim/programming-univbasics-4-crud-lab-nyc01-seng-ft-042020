b57444bf01ea6bc8cf0ee716480224035b16ef4a
Joshua Kim <jykim92@gmail.com>
2020-04-08 16:19:27 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -30,15 +30,18 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-
+  array = ["wow", "hello", "am", "later", "farewell", "arrays!"]
+  array[2]
 end
 
 def retrieve_first_element_from_array(array)
-
+  array = ["wow", "hello", "am", "later", "farewell", "arrays!"]
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-
+  array = ["wow", "hello", "am", "later", "farewell", "arrays!"]
+  array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
